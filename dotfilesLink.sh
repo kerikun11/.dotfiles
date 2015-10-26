19e1cb4eb0a8a5e98a1499756a3833c0da13d3ea
kerikun11 <kerikun11@gmail.com>
2015-10-27 00:52:51 +0900

    .vim

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+echo "making .vimrc"
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+echo "installing NeoBundle"
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
