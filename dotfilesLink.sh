63f6893598f3ed2253d506659adbd023f8099894
kerikun11 <kerikun11@gmail.com>
2015-10-30 09:40:35 +0900

    add link of .zshrc to dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,6 +3,9 @@
 echo "making .vimrc"
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
+echo "making .zshrc"
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
 echo "installing NeoBundle"
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
