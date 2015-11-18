f6579e11504ac83ad501d2bf7de067cc675c37d2
kerikun11 <kerikun11@gmail.com>
2015-11-18 22:50:32 +0900

    git clone neobundle in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 手動管理
-DOTFILES=(.vimrc .zshrc .tmux.conf .gitconfig .vim)
+DOTFILES=(.vimrc .zshrc .tmux.conf .gitconfig)
 
 # このディレクトリを原点に環境を構築する
 # ので、スクリプトのテストする時はテスト用のディレクトリに変えて
@@ -11,4 +11,8 @@ for file in ${DOTFILES[@]}; do
 	echo $file
 	ln -sfd $HOME/dotfiles/$file $ROOT_DIR/$file
 done
+
+echo "Git clone neobundle"
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
