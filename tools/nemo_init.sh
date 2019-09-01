40eeaa925d7f2bc07611a46a6a88238375f9297d
Ryotaro Onuki <kerikun11+github@gmail.com>
2019-09-01 15:23:24 +0900

    add nemo init

---
+++ tools/nemo_init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# vscode here in nemo
+cp $HOME/.dotfiles/config/nemo/actions/vscode.nemo_action \
+   $HOME/.local/share/nemo/actions/vscode.nemo_action
+
+# vscode icon
+sudo ln -sf "/usr/share/code/resources/app/resources/linux/code.png" "/usr/local/share/icons/code.png"
