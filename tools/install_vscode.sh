543b265035a32b36e8bfa9beff0ef3e41226674e
Ryotaro Onuki <kerikun11+github@gmail.com>
2019-10-29 20:45:28 +0900

    add libinput-gestures

---
+++ tools/install_vscode.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -x
+set -e
+
+## see https://code.visualstudio.com/docs/setup/linux
+wget -O- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >/tmp/packages.microsoft.gpg
+sudo install -o root -g root -m 644 /tmp/packages.microsoft.gpg /usr/share/keyrings/
+rm /tmp/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install -y apt-transport-https
+sudo apt install -y code
+# vscode icon
+sudo mkdir -p /usr/local/share/icons
+sudo ln -sf "/usr/share/code/resources/app/resources/linux/code.png" "/usr/local/share/icons/code.png"
--- tools/mint_setup.sh
+++ tools/mint_setup.sh
@@ -17,19 +17,15 @@ sudo apt install -y git zsh curl vim
 $HOME/.dotfiles/install.sh
 
 ## VSCode
-## https://code.visualstudio.com/docs/setup/linux
-wget -O- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >/tmp/packages.microsoft.gpg
-sudo install -o root -g root -m 644 /tmp/packages.microsoft.gpg /usr/share/keyrings/
-rm /tmp/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt update
-sudo apt install -y apt-transport-https
-sudo apt install -y code
-# vscode icon
-sudo mkdir -p /usr/local/share/icons
-sudo ln -sf "/usr/share/code/resources/app/resources/linux/code.png" "/usr/local/share/icons/code.png"
+$HOME/.dotfiles/tools/install_vscode.sh
 
 ## TeX-Live
 sudo apt install -y texlive-full
+# error avoidance
 rm -rf $HOME/.cpan
 sudo cpan Log::Dispatch::File File::HomeDir
+
+## Inkscape
+sudo add-apt-repository ppa:inkscape.dev/stable
+sudo apt-get update
+sudo apt install -y inkscape
