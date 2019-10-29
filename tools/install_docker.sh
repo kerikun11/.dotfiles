5fe2244579590fa80aaf5571a24bd7fb8215054c
Ryotaro Onuki <kerikun11+github@gmail.com>
2019-10-29 15:37:35 +0900

    add docker dependency

--- tools/install_docker.sh
+++ tools/install_docker.sh
@@ -4,7 +4,7 @@ set -x
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y docker.io
+sudo apt install -y docker.io cgroupfs-mount
 sudo cgroupfs-mount
 sudo usermod -aG docker $USER
 sudo service docker start
