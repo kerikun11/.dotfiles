30c2cf07f1d6cdf7f6a973da565bcdca962dd81f
Ryotaro Onuki <kerikun11+github@gmail.com>
2020-04-12 01:48:26 +0900

    update gsettings

--- tools/manjaro_setup.sh
+++ tools/manjaro_setup.sh
@@ -11,8 +11,30 @@ LANG=C xdg-user-dirs-gtk-update
 gsettings set org.gnome.libgnomekbd.keyboard options "['ctrl\tctrl:nocaps']"
 echo "OK capslock"
 
-## hide desktop entry
-# gsettings set org.nemo.desktop show-desktop-icons false
+## Cinnamon
+gsettings set org.cinnamon.settings-daemon.peripherals.touchpad horizontal-scrolling true
+gsettings set org.cinnamon.desktop.screensaver lock-enabled false
+gsettings set org.cinnamon.settings-daemon.plugins.power lid-close-ac-action 'nothing'
+gsettings set org.cinnamon.settings-daemon.plugins.power lid-close-battery-action 'nothing'
+gsettings set org.cinnamon.settings-daemon.plugins.power idle-dim-time 30
+gsettings set org.cinnamon.settings-daemon.plugins.power idle-brightness 5
+gsettings set org.cinnamon.settings-daemon.plugins.power sleep-display-ac 3600               # sleep diplay
+gsettings set org.cinnamon.settings-daemon.plugins.power sleep-display-battery 300           # sleep diplay
+gsettings set org.cinnamon.settings-daemon.plugins.power sleep-inactive-battery-timeout 3600 # sleep PC
+echo "OK Cinnamon"
+
+## Nemo
+gsettings set org.nemo.desktop show-desktop-icons false
+gsettings set org.nemo.preferences default-folder-viewer 'list-view'
+gsettings set org.nemo.window-state start-with-menu-bar false
+## tool bar
+gsettings set org.nemo.preferences show-compact-view-icon-toolbar true
+gsettings set org.nemo.preferences show-open-in-terminal-toolbar true
+gsettings set org.nemo.preferences show-home-icon-toolbar true
+gsettings set org.nemo.preferences show-new-folder-icon-toolbar true
+gsettings set org.nemo.preferences show-edit-icon-toolbar true
+gsettings set org.nemo.preferences show-computer-icon-toolbar true
+echo "OK nemo"
 
 ## Package Source Repository
 read -p "update pacman mirrors? [y/N] :" YN
@@ -47,21 +69,8 @@ $yay_install_cmd google-chrome
 $yay_install_cmd slack-desktop
 ## Dropbox
 $yay_install_cmd dropbox
-echo "OK packages"
-
-## Nemo
 $yay_install_cmd nemo-dropbox
-gsettings set org.nemo.desktop show-desktop-icons false
-gsettings set org.nemo.preferences default-folder-viewer 'list-view'
-gsettings set org.nemo.window-state start-with-menu-bar false
-## tool bar
-gsettings set org.nemo.preferences show-compact-view-icon-toolbar true
-gsettings set org.nemo.preferences show-open-in-terminal-toolbar true
-gsettings set org.nemo.preferences show-home-icon-toolbar true
-gsettings set org.nemo.preferences show-new-folder-icon-toolbar true
-gsettings set org.nemo.preferences show-edit-icon-toolbar true
-gsettings set org.nemo.preferences show-computer-icon-toolbar true
-echo "OK nemo"
+echo "OK packages"
 
 ## Fcitx and Mozc
 $yay_install_cmd fcitx-mozc fcitx-im
