3d7e7101575e22c8d4f901896478c23d0c0046aa
Ryotaro Onuki <kerikun11+github@gmail.com>
2020-05-06 21:27:46 +0900

    add script to make sdkconfig default of esp-idf

---
+++ tools/update_sdkconfig_defaults.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# A script that updates sdkconfig.defaults
+# to be the smallest in the current ESP-IDF version.
+
+# command to generate sdkconfig of esp-idf
+IDF_RECONFIGURE="$IDF_PATH/tools/idf.py reconfigure"
+
+# update sdkconfig of the project
+$IDF_RECONFIGURE
+mv sdkconfig sdkconfig.project
+
+# generate clean sdkconfig of current esp-idf version
+rm -f sdkconfig.defaults
+$IDF_RECONFIGURE
+mv sdkconfig sdkconfig.clean
+
+# generate sdkconfig.defaults with diff command
+diff --old-line-format='' --new-line-format='%L' --unchanged-line-format='' \
+    sdkconfig.clean sdkconfig.project \
+    >sdkconfig.defaults
+# remove comment lines
+sed -i '/^# .*/d' sdkconfig.defaults
+
+# place sdkconfig
+mv sdkconfig.project sdkconfig
+
+# cleaning
+rm sdkconfig.clean
+
+echo 'updated sdkconfig and sdkconfig.default to current ESP-IDF version'
