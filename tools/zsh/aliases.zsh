ee7ad3fad056715288a799428736755deafeefa4
Ryotaro Onuki <kerikun11+github@gmail.com>
2021-06-19 22:22:42 +0900

    update format

--- tools/zsh/aliases.zsh
+++ tools/zsh/aliases.zsh
@@ -3,53 +3,54 @@
 ## date: 2021.06.01
 ##============================================================================##
 ## custom aliases
-alias od-ascii="od -tx1z -Ax"
-alias serve="python3 -m http.server"
-alias takebuild="take build" # call oh-my-zsh function
-alias zshenv="$EDITOR $HOME/.zshenv"
-alias zshrc="$EDITOR $HOME/.zshrc"
-alias gsettings_list="gsettings list-recursively"
+alias od-ascii='od -tx1z -Ax'
+alias serve='python3 -m http.server'
+alias takebuild='take build' # call oh-my-zsh function
+alias zshenv='$EDITOR $HOME/.zshenv'
+alias zshrc='$EDITOR $HOME/.zshrc'
+alias gsettings_list='gsettings list-recursively'
 
 ## initial aliases
-alias c="code"
+alias c='code'
 alias cdg='cd $(git rev-parse --show-toplevel)'
-alias dr="docker"
-alias drb="docker build"
-alias drcs="docker-compose"
-alias dri="docker images"
-alias drr="docker run"
-alias drrrm="docker run --rm -i -t"
-alias drrme="docker run --rm -i -t --network host -u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v $PWD:/w -w /w"
-alias e="echo"
-alias g="git"
-alias gg="git log --graph --oneline"
-alias gga="git log --graph --oneline --all"
+alias dr='docker'
+alias drb='docker build'
+alias drcs='docker-compose'
+alias dri='docker images'
+alias drr='docker run'
+alias drrrm='docker run --rm -i -t'
+alias drrme='docker run --rm -i -t --network host -u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v $PWD:/w -w /w'
+alias drrx11='docker run --rm -i -t --network host -u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v $PWD:/w -w /w -v $HOME/.Xauthority:$HOME/.Xauthority:ro -e DISPLAY'
+alias e='echo'
+alias g='git'
+alias gg='git log --graph --oneline'
+alias gga='git log --graph --oneline --all'
 alias gu='echo "$(git config --get user.name) <$(git config --get user.email)>"'
-alias h="history"
-alias hs="history | grep -i"
-alias kd="echo -n 'source <(curl -fsSL kerislab.jp/d)' | clipcopy"
-alias l="ls -lha"
-alias la="ls -la"
-alias ll="ls -l"
-alias ls="ls --color=auto"
-alias lsa="ls -a"
-alias m="make"
-alias n="ninja"
-alias o="open"
-alias p="python"
-alias p2="python2"
-alias p3="python3"
-alias r="exec zsh" # reload zsh
-alias sv="serve"
-alias tb="take build" # call oh-my-zsh function
-alias wh="which"
+alias h='history'
+alias hs='history | grep -i'
+alias kd='echo -n "source <(curl -fsSL kerislab.jp/d)" | clipcopy'
+alias l='ls -lha'
+alias la='ls -la'
+alias ll='ls -l'
+alias ls='ls --color=auto'
+alias lsa='ls -a'
+alias m='make'
+alias n='ninja'
+alias o='open'
+alias p='python'
+alias p2='python2'
+alias p3='python3'
+alias r='exec zsh' # reload zsh
+alias sv='serve'
+alias tb='take build' # call oh-my-zsh function
+alias wh='which'
 
 ## command replace
-alias make="make -j$(nproc)"
-alias open="open_command" # call oh-my-zsh function
-type bat &>/dev/null && alias cat="bat"
-type exa &>/dev/null && alias ls="exa -g"
-type trash-put &>/dev/null && alias rm="trash-put"
+alias make='make -j$(nproc)'
+alias open='open_command' # call oh-my-zsh function
+type bat &>/dev/null && alias cat='bat'
+type exa &>/dev/null && alias ls='exa -g'
+type trash-put &>/dev/null && alias rm='trash-put'
 
 ## global aliases
 alias -g A='2>&1 | tee -a'
@@ -64,30 +65,30 @@ alias -g W='| wc -l'
 alias -g X='| xargs'
 
 ## date string
-alias datestr="date +%Y%m%d"
-alias datetimestr="date +%Y%m%d-%H%M%S"
+alias datestr='date +%Y%m%d'
+alias datetimestr='date +%Y%m%d-%H%M%S'
 
 ## long command alias
-alias upgradeall="sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean"
+alias upgradeall='sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean'
 alias sp2ub='find . -name "* *" | rename -v "s/ /_/g"'
 
 ## esp-idf alias
 alias idf_='export IDF_PATH=/usr/local/Espressif/esp-idf    && . $IDF_PATH/export.sh'
 alias idf3='export IDF_PATH=/usr/local/Espressif/esp-idf_v3 && . $IDF_PATH/export.sh'
 alias idf4='export IDF_PATH=/usr/local/Espressif/esp-idf_v4 && . $IDF_PATH/export.sh'
-alias idf="idf.py"
-alias idfbuild="idf.py build"
-alias idfflashmonitor="idf.py -b 2000000 flash monitor"
-alias idfmonitor="idf.py monitor"
-alias idfmenuconfig="idf.py menuconfig"
+alias idf='idf.py'
+alias idfbuild='idf.py build'
+alias idfflashmonitor='idf.py -b 2000000 flash monitor'
+alias idfmonitor='idf.py monitor'
+alias idfmenuconfig='idf.py menuconfig'
 
 ## PDF n-up
-alias pdfnup2x1="pdfnup --batch --a4paper --nup 2x1 --scale 1.0  --landscape"
-alias pdfnup2x2="pdfnup --batch --a4paper --nup 2x2 --scale 0.96 --landscape"
-alias pdfnup2x4="pdfnup --batch --a4paper --nup 2x4 --scale 0.96 --no-landscape"
+alias pdfnup2x1='pdfnup --batch --a4paper --nup 2x1 --scale 1.0  --landscape'
+alias pdfnup2x2='pdfnup --batch --a4paper --nup 2x2 --scale 0.96 --landscape'
+alias pdfnup2x4='pdfnup --batch --a4paper --nup 2x4 --scale 0.96 --no-landscape'
 
 ## Exiftool
-alias rmgeotag="exiftool -overwrite_original -geotag="
+alias rmgeotag='exiftool -overwrite_original -geotag='
 
 ##============================================================================##
 ## directory alias
@@ -113,15 +114,15 @@ function datetimediff() {
   read te
   ts=$(date -d "${ts:0:19}" +%s)
   te=$(date -d "${te:0:19}" +%s)
-  echo ""
+  echo
   echo "$((te - ts)) [s]"
   echo "$(((te - ts) / 3600)):$(date --utc +"%M:%S" -d @$(($te - $ts)))"
 }
 function make-dockerfile-runnable() {
-  alias ARG="export"
-  alias ENV="export"
-  alias RUN=""
-  alias COPY="rsync -av"
+  alias ARG='export'
+  alias ENV='export'
+  alias RUN=''
+  alias COPY='rsync -av'
   echo '$ source Dockerfile'
 }
 function periodic-open() {
