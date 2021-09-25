70da711a1dd4768c4106c0529d04327bde1d3679
Ryotaro Onuki <kerikun11+github@gmail.com>
2021-09-26 01:32:01 +0900

    update nemo settings

--- tools/cinnamon_setup.sh
+++ tools/cinnamon_setup.sh
@@ -30,15 +30,17 @@ echo "OK Cinnamon"
 
 ## Nemo
 gsettings set org.nemo.desktop show-desktop-icons false
+gsettings set org.nemo.preferences click-double-parent-folder true
 gsettings set org.nemo.preferences default-folder-viewer 'list-view'
 gsettings set org.nemo.window-state start-with-menu-bar false
 ## tool bar
 gsettings set org.nemo.preferences show-compact-view-icon-toolbar true
-gsettings set org.nemo.preferences show-computer-icon-toolbar true
+gsettings set org.nemo.preferences show-computer-icon-toolbar false
 gsettings set org.nemo.preferences show-edit-icon-toolbar true
-gsettings set org.nemo.preferences show-home-icon-toolbar true
+gsettings set org.nemo.preferences show-home-icon-toolbar false
 gsettings set org.nemo.preferences show-new-folder-icon-toolbar true
 gsettings set org.nemo.preferences show-open-in-terminal-toolbar true
+gsettings set org.nemo.preferences show-reload-icon-toolbar true
 echo "OK nemo"
 
 ## GNOME Terminal
